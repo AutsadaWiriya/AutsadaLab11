30afd660d732878c1e4140de826afd2f48a45c2e
AutsadaWiriya <aut0935261784@gmail.com>
2023-01-03 15:31:53 +0700

    Add

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,10 +9,14 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline;
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while(getline(source, textline)){
+		dest << textline << endl ;
+	}
+	dest << "-------------------- HA!! ---------------------" ;
 
-    source.close();
-    dest.close();
+   source.close();
+   dest.close();
 	return 0;
 }
