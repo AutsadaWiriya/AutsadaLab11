18d36d35501768b02a230026e1bd7c0b2ac03b32
AutsadaWiriya <aut0935261784@gmail.com>
2023-01-03 16:18:49 +0700

    Add

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include<fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+   int N = 0;
+   double mean, stddev, sum = 0 ,sumpow = 0;
+   ifstream score;
+   score.open("score.txt");
+   
+   string textline;
+   while(getline(score, textline)){
+      sum = sum + atof(textline.c_str());
+      sumpow = sumpow + pow(atof(textline.c_str()), 2);
+		N++;
+	}
+   mean = sum/N ;
+   stddev = sqrt( (sumpow/N ) - mean*mean );
+
+   cout << "Number of data = " << N << endl;
+   cout << setprecision(3);
+   cout << "Mean = " << mean << endl;
+   cout << "Standard deviation = " << stddev << endl;
+}
